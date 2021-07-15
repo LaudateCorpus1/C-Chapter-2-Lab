f5b2d5577a8d71524e87ab0780fe9d5372f5b344
LaudateCorpus1 <you@example.com>
2021-07-14 21:17:37 -0700

    master

---
+++ Midterm/Write a Bank Charges program.cpp
@@ -0,0 +1,16 @@
+Write a Bank Charges program
+
+The program asks for the beginning balance and the number of checks written. Compute and display the banks's service fees for the month.
+
+Requirements:
+
+A bank charges $10 per month plus the following check fees for a commercial checking account:
+$1 each for fewer than 10 checks
+$.75 each for 10-29 checks
+$.50 each for 30-49 checks
+$.25 each for 50 or more checks
+The bank also charges an extra $25 if the balance of the account falls below $500 (before any check fees are applied).
+Use validation for the input. Do no accept a negative value for the number of checks written. If a negative value is given for the beginning balance, display an urgent message indicating the account is overdrawn.
+Use of function in your program (see Chapter 6)
+Use "iomanip"  to format the output and $ for amount.  I would like to see the details output, like the receipt.
+Note: Midterm is to be done individually. You may not copy someone else’s work. I adhere to the Ohlone College Policy on Academic Integrity.
